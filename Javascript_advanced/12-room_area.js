facb734bfc53acf13a7622a73d842f7c7db8c402
hug0-cstrs <hugo.casteras@outlook.fr>
2024-01-23 09:47:23 +0000

    12. Binding

--- Javascript_advanced/12-room_area.js
+++ Javascript_advanced/12-room_area.js
@@ -1,10 +1,10 @@
 roomDimensions = {
   width: 50,
   length: 100,
-  getArea: function() {
+  getArea: function () {
     return this.width * this.length;
-  }
-}
+  },
+};
 
 // Function context becomes global scope
 const GetAreaGlobal = roomDimensions.getArea;
